9d10329c509104983c01775575cda3e0d3af54c9
daxferab <daxferab.19@gmail.com>
2025-03-18 01:52:20 +0100

    Fix: cmd_echo call from inputhandler

--- src/inputhandler.c
+++ src/inputhandler.c
@@ -6,7 +6,7 @@
 /*   By: daxferna <daxferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 13:03:56 by daxferna          #+#    #+#             */
-/*   Updated: 2025/03/17 02:06:18 by daxferna         ###   ########.fr       */
+/*   Updated: 2025/03/18 01:51:54 by daxferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	input_handler(t_smash *smash, char	**input)
 	if (ft_str_equals(input[0], "env"))
 		cmd_env(*smash);
 	else if (ft_str_equals(input[0], "pwd"))
-		cmd_pwd(*smash);
+		cmd_pwd(smash);
 	else if (ft_str_equals(input[0], "unset"))
 		cmd_unset(smash, input);
 	else if (ft_str_equals(input[0], "export"))
 		cmd_export(smash, input);
 	else if (ft_str_equals(input[0], "echo"))
-		cmd_echo(input);
+		cmd_echo(&input);
 	else if (ft_str_equals(input[0], "cd"))
 		cmd_cd(); //TODO: Create function
 	else if (ft_str_equals(input[0], "exit"))
