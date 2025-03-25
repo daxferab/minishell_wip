d5e0ad2e3af10d962d7593841fd2910d2f574b1d
daxferab <daxferab.19@gmail.com>
2025-03-25 19:20:37 +0100

    Fix: cd function

--- src/builtins/cd_builtin.c
+++ src/builtins/cd_builtin.c
@@ -6,36 +6,56 @@
 /*   By: daxferna <daxferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 11:15:28 by daxferna          #+#    #+#             */
-/*   Updated: 2025/03/21 19:19:21 by daxferna         ###   ########.fr       */
+/*   Updated: 2025/03/25 19:20:07 by daxferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	cmd_cd(t_smash *smash, char **input) //FIXME
+static void update_wd(t_smash *smash);
+static int	array_len(char **array);
+
+bool	cmd_cd(t_smash *smash, char **input)
 {
-	if (input[2]) //too many args
+	int	cmd_len;
+
+	cmd_len = array_len(input);
+	if (cmd_len > 2) //too many args
 		return (false);
-	if (!input[1]) 
+	if (cmd_len == 1) //no args
 	{
 		if (!get_value(smash->envp, "HOME"))
-			return (false);
-		chdir(get_value(smash->envp, "HOME"));
-		update_envp(&smash->envp, "OLDPWD", smash->cwd);
-		if (smash->cwd)
-			free(smash->cwd);
-		smash->cwd = getcwd(NULL, 0);
-		update_envp(&smash->envp, "PWD", smash->cwd);
+			return (false); //HOME not set
+		if (chdir(get_value(smash->envp, "HOME")) == -1)
+			return (false); //error changing directory
+		update_wd(smash);
 		return (true);
 	}
 	if (access(input[1], F_OK | X_OK) == -1) //file does not exist
-		return (false);
+		return (false); // Not an existing directory
 	if (chdir(input[1]) == -1) //error changing directory
 		return (false);
-	update_envp(&smash->envp, "OLDPWD", smash->cwd);
-	if (smash->cwd)
-		free(smash->cwd);
-	smash->cwd = getcwd(NULL, 0);
-	update_envp(&smash->envp, "PWD", smash->cwd);
+	update_wd(smash);
 	return (true);
+}
+
+static void update_wd(t_smash *smash) //FIXME
+{
+	update_envp(&smash->envp, "OLDPWD", get_value(smash->envp, "PWD"));
+	// update_envp(&smash->envp, "OLDPWD", "");
+	// if (smash->cwd)
+	// 	free(smash->cwd);
+	// smash->cwd = getcwd(NULL, 0);
+	// if (get_value(smash->envp, "PWD"))
+	// 	update_envp(&smash->envp, "PWD", smash->cwd);
+}
+
+static int	array_len(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+		i++;
+	return (i);
 }
