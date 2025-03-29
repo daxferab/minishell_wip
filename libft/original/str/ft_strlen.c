69aebf5a8e853ada998b68db9f94d5c728125695
Pablo <pablodelolmodecasas@gmail.com>
2025-03-29 17:27:26 +0100

    protect strlen

--- libft/original/str/ft_strlen.c
+++ libft/original/str/ft_strlen.c
@@ -6,14 +6,14 @@
 /*   By: pdel-olm <pdel-olm@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 19:59:32 by pdel-olm          #+#    #+#             */
-/*   Updated: 2024/10/18 16:13:17 by pdel-olm         ###   ########.fr       */
+/*   Updated: 2025/03/29 16:40:33 by pdel-olm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-Computes the length of the string s. 
+Computes the length of the string s.
 The strnlen() function attempts to compute the length of s,
 	but never scans beyond the first maxlen bytes of s.
 */
@@ -22,7 +22,8 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (s[i])
-		i++;
+	if (s)
+		while (s[i])
+			i++;
 	return (i);
 }
