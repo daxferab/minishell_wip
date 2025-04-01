02c48bfe766d32691af68a89c20cf9694cb88ae1
daxferab <daxferab.19@gmail.com>
2025-04-01 19:47:17 +0200

    Add: Error message

--- src/builtins/cmd_env.c
+++ src/builtins/cmd_env.c
@@ -3,7 +3,7 @@
 int	cmd_env(t_smash	smash, char **input)
 {
 	if (input[1])
-		return (2);
+		return (ft_putstr_fd("smash: env: too many args\n", 2), 2);
 	display_envp(smash.envp);
 	return (0);
 }
