2d94416a905299519d87b8dc9300c0aacfea339f
daxferab <daxferab.19@gmail.com>
2025-04-04 19:15:59 +0200

    Add: get_cmd_num

--- src/parse/parse_pipeline.c
+++ src/parse/parse_pipeline.c
@@ -1,22 +1,32 @@
 #include "minishell.h"
 
+static int	get_cmd_num(t_token *current)
+{
+	int		cmd_num;
+
+	t_token	*before;
+	cmd_num = 0;
+	before = NULL;
+	while (current->next || current->type == PIPE)
+	{
+		if (is_word(current->type) && (!before || !is_redirection(before->type)))
+		{
+			ft_printf_fd(STDERR_FILENO, "Word: %s\n", current->value);
+			cmd_num++;
+		}
+		before = current;
+		current = current->next;
+	}
+	return (cmd_num);
+}
+
 void	parse_pipeline(t_smash *smash)
 {
 	int		cmd_num;
 	t_token	*current;
-	t_token	*before;
 	
 	current = smash->first_token;
-	while (current->next)
-	{
-		cmd_num = 0;
-		before = NULL;
-		while (current->next && current->next->type != PIPE)
-		{
-			if (is_word(current->type) && (!before || !is_redirection(before->type)))
-				cmd_num++;
-			before = current;
-			current = current->next;
-		}
-	}
+	cmd_num = get_cmd_num(current);
+	debug_int(smash, "cmd_num", cmd_num);
+	(void)cmd_num;
 }
