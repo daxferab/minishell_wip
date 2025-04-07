894bacb9a8e9dc7c180eff5f489268b6225a3b98
daxferab <daxferab.19@gmail.com>
2025-04-07 18:17:34 +0200

    STASH: wic

--- src/parse/parse_pipeline.c
+++ src/parse/parse_pipeline.c
@@ -1,5 +1,49 @@
 #include "minishell.h"
 
+static int	get_cmd_num(t_token *current);
+static void	new_pipeline(t_pipelines *pipelst, t_token *current);
+
+void	parse_pipeline(t_smash *smash)
+{
+	int			cmd_num;
+	int			i;
+	t_pipelines	*pipelst;
+	t_token		*current;
+
+	pipelst = NULL;
+	current = smash->first_token;
+	while (current)
+	{
+		new_pipeline(pipelst, current);
+		while (current && current->type != PIPE)
+			current = current->next;
+	}
+	smash->first_redir = pipelst;
+}
+
+static void	new_pipeline(t_pipelines *pipelst, t_token *current)
+{
+	int			cmd_num;
+	int			i;
+	t_pipelines	*new_pipeline;
+
+	cmd_num = get_cmd_num(current);
+	i = 0;
+	new_pipeline = malloc(sizeof(t_pipelines));
+	new_pipeline->cmd = malloc(sizeof(char *) * (cmd_num + 1)); //TODO: protect
+	while (current && current->type != PIPE)
+	{
+		if (is_redirection(current->type))
+		{
+			add_redirection(new_pipeline, current); //TODO: create this function
+			current = current->next;
+		}
+		else
+			new_pipeline->cmd[i++] = current->value;
+		current = current->next;
+	}
+}
+
 static int	get_cmd_num(t_token *current)
 {
 	int		cmd_num;
@@ -10,23 +54,9 @@ static int	get_cmd_num(t_token *current)
 	while (current && current->type != PIPE)
 	{
 		if (is_word(current->type) && (!before || !is_redirection(before->type)))
-		{
-			ft_printf_fd(STDERR_FILENO, "Word: %s\n", current->value);
 			cmd_num++;
-		}
 		before = current;
 		current = current->next;
 	}
 	return (cmd_num);
-}
-
-void	parse_pipeline(t_smash *smash)
-{
-	int		cmd_num;
-	t_token	*current;
-	
-	current = smash->first_token;
-	cmd_num = get_cmd_num(current);
-	debug_int(smash, "cmd_num", cmd_num);
-	(void)cmd_num;
 }
