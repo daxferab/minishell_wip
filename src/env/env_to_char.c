6a707dfd16d3c1ed3e34713f461a9acaa9144a24
Pablo <pablodelolmodecasas@gmail.com>
2025-04-26 14:35:54 +0200

    fix env_to_char

--- src/env/env_to_char.c
+++ src/env/env_to_char.c
@@ -1,5 +1,29 @@
 #include "minishell.h"
 
+static int	lstlen(t_envp *lst);
+static char	*create_line(char *s1, char *s2, char *s3);
+
+char	**env_to_char(t_envp *env_lst)
+{
+	char	**envp;
+	int		i;
+
+	i = 0;
+	envp = malloc((lstlen(env_lst) + 1) * sizeof(t_envp));
+	if (!envp)
+		return (ft_putstr_fd("Malloc error", 2), NULL);
+	while (env_lst)
+	{
+		envp[i] = create_line(env_lst->key, "=", env_lst->value);
+		if (!envp[i])
+			return (ft_free_double_pointer((void **)envp), NULL);
+		env_lst = env_lst->next;
+		i++;
+	}
+	envp[i] = NULL;
+	return (envp);
+}
+
 static int	lstlen(t_envp *lst)
 {
 	int	count;
@@ -15,42 +39,17 @@ static int	lstlen(t_envp *lst)
 	return (count);
 }
 
-static	char	*create_line(char *s1, char *s2, char *s3)
+static char	*create_line(char *s1, char *s2, char *s3)
 {
+	char	*aux;
 	char	*line;
 
-	line = ft_strjoin(s1, s2);
-	if (!line)
+	aux = ft_strjoin(s1, s2);
+	if (!aux)
 		return (ft_putstr_fd("Malloc error", 2), NULL);
-	line = ft_strjoin(line, s3);
+	line = ft_strjoin(aux, s3);
+	free(aux);
 	if (!line)
 		return (ft_putstr_fd("Malloc error", 2), NULL);
 	return (line);
-}
-
-char	**env_to_char(t_envp *env_lst)
-{
-	char	**envp;
-	char	*line;
-	int		i;
-
-	i = 0;
-	envp = malloc((lstlen(env_lst) + 1) * sizeof(t_envp));
-	if (!envp)
-		return (ft_putstr_fd("Malloc error", 2), NULL);
-	while (env_lst)
-	{
-		line = create_line(env_lst->key, "=", env_lst->value);
-		if (!line)
-			return (ft_free_double_pointer((void **)envp), NULL);
-		envp[i] = ft_strdup(line);
-		if (!envp[i])
-			return (ft_putstr_fd("Malloc error", 2), free(line),\
-			ft_free_double_pointer((void **)envp), NULL);
-		free(line);
-		env_lst = env_lst->next;
-		i++;
-	}
-	envp[i] = NULL;
-	return (envp);
 }
