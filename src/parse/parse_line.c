46e69858d23a8b1b69b775d8443704c7615022c0
daxferab <daxferab.19@gmail.com>
2025-05-17 01:11:07 +0200

    Update: parse_line empty prompt

--- src/parse/parse_line.c
+++ src/parse/parse_line.c
@@ -4,13 +4,16 @@ bool	parse_line(t_smash *smash)
 {
 	smash->user_input = prompt(smash, false);
 	if (!smash->user_input || !smash->user_input[0])
-		return (true);
-	add_history_entry(smash);
-	if (!tokenize(smash) || !expand_variables(smash) || !remove_quotes(smash))
-		return (ft_putstr_fd("Internal error\n", 2), clear_input(smash), false);
-	if (!syntax(smash))
-		return (true); //TODO: Handle syntax error
-	if (!parse_pipeline(smash))
-		return (ft_putstr_fd("Internal error\n", 2), clear_input(smash), false);
+		smash->error_type = EMPTY_PROMPT;
+	else
+	{
+		add_history_entry(smash);
+		if (!tokenize(smash) || !expand_variables(smash) || !remove_quotes(smash))
+			return (ft_putstr_fd("Internal error\n", 2), clear_input(smash), false);
+		if (!syntax(smash))
+			return (true); //TODO: Handle syntax error
+		if (!parse_pipeline(smash))
+			return (ft_putstr_fd("Internal error\n", 2), clear_input(smash), false);
+	}
 	return (true);
 }
