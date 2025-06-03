be09f8deefd4ab69f45ae4a82d39135e356ed269
Pablo <pablodelolmodecasas@gmail.com>
2025-06-03 21:53:19 +0200

    cooler prompt

--- src/control/prompt.c
+++ src/control/prompt.c
@@ -1,35 +1,6 @@
 #include "minishell.h"
 
-//TODO refactor to admit at least \001 and \002, maybe all?
-//TODO calculate chars beforehand and allocate just for those
-//TODO free
-char	*parse_octals(char *prompt)
-{
-	char	*new;
-	int		prompt_iter;
-	int		new_iter;
-
-	new = ft_strdup(prompt);
-	prompt_iter = 0;
-	new_iter = 0;
-	while (prompt[prompt_iter])
-	{
-		if (prompt[prompt_iter] == '\\' && prompt[prompt_iter + 1] == '0' && prompt[prompt_iter + 2] == '3' && prompt[prompt_iter + 3] == '3')
-		{
-			new[new_iter] = '\033';
-			new_iter++;
-			prompt_iter += 4;
-		}
-		else
-		{
-			new[new_iter] = prompt[prompt_iter];
-			new_iter++;
-			prompt_iter++;
-		}
-	}
-	new[new_iter] = '\0';
-	return (new);
-}
+static char	get_char(char *c);
 
 char	*prompt(t_smash *smash, bool in_heredoc)
 {
@@ -47,11 +18,57 @@ char	*prompt(t_smash *smash, bool in_heredoc)
 		if (!prompt)
 			prompt = PROMPT;
 	}
-	//prompt = parse_octals(prompt);
 	if (smash->debug_mode)
 	{
-		ft_printf(prompt);
+		ft_printf("%s", prompt);
 		return (ft_get_next_line_no_nl(STDIN_FILENO));
 	}
 	return (readline(prompt));
+}
+
+//TODO free, or just call after export
+char	*parse_octals_prompt(char *prompt)
+{
+	char	*new;
+	int		prompt_iter;
+	int		new_iter;
+
+	prompt_iter = -1;
+	new_iter = 0;
+	while (prompt[++prompt_iter])
+	{
+		new_iter++;
+		if (prompt[prompt_iter] != get_char(&(prompt[prompt_iter])))
+			prompt_iter += 3;
+	}
+	new = ft_calloc(new_iter + 1, sizeof(char));
+	prompt_iter = -1;
+	new_iter = 0;
+	while (prompt[++prompt_iter])
+	{
+		new[new_iter++] = get_char(&prompt[prompt_iter]);
+		if (prompt[prompt_iter] != get_char(&(prompt[prompt_iter])))
+			prompt_iter += 3;
+	}
+	return (new);
+}
+
+static char	get_char(char *c)
+{
+	if (*c && *c == '\\'
+		&& *(c + 1) && *(c + 1) == '0'
+		&& *(c + 2) && *(c + 2) == '0'
+		&& *(c + 3) && *(c + 3) == '1')
+		return ('\001');
+	if (*c && *c == '\\'
+		&& *(c + 1) && *(c + 1) == '0'
+		&& *(c + 2) && *(c + 2) == '0'
+		&& *(c + 3) && *(c + 3) == '2')
+		return ('\002');
+	if (*c && *c == '\\'
+		&& *(c + 1) && *(c + 1) == '0'
+		&& *(c + 2) && *(c + 2) == '3'
+		&& *(c + 3) && *(c + 3) == '3')
+		return ('\033');
+	return (*c);
 }
