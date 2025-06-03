f97a3b846f2f67476a601cbbda1721a2beac5b5a
Pablo <pablodelolmodecasas@gmail.com>
2025-06-03 19:42:58 +0200

    divided tokenize

--- src/parse/tokenize.c
+++ src/parse/tokenize.c
@@ -1,44 +1,52 @@
 #include "minishell.h"
 
-static bool			is_start_token(int start_token, char c);
-static bool			is_end_token(int start_token, char c, t_token_type type);
-static bool			add_token(t_smash *smash,
-						int start, int len, t_token_type type);
+static bool	iter_input(t_smash *smash, int *start_token);
+static bool	is_start_token(int start_token, char c);
+static bool	is_end_token(int start_token, char c, t_token_type type);
+static bool	add_token(t_smash *smash, int start, int len, t_token_type type);
 
 bool	tokenize(t_smash *smash)
 {
-	int				iter;
-	int				start_token;
-	t_token_type	type;
+	int	start_token;
 
-	iter = -1;
 	start_token = -1;
-	type = -1;
-	while (smash->user_input[++iter])
+	if (!iter_input(smash, &start_token))
 	{
-		if (is_start_token(start_token, smash->user_input[iter]))
-		{
-			get_token_type(&type, &(smash->user_input[iter]));
-			start_token = iter;
-			if (type == HEREDOC || type == APPEND)
-				iter++;
-		}
-		mutate(&type, smash->user_input[iter]);
-		if (is_end_token(start_token, smash->user_input[iter + 1], type))
-		{
-			if (!add_token(smash, start_token, iter - start_token + 1, type))
-			{
-				smash->error_type = INTERNAL;
-				return (false);
-			}
-			start_token = -1;
-		}
+		smash->error_type = INTERNAL;
+		return (false);
 	}
 	if (start_token != -1)
 	{
 		ft_printf_fd(STDERR_FILENO, "smash: unclosed quotes\n");
 		smash->error_type = UNCLOSED_QUOTES;
 		return (false);
+	}
+	return (true);
+}
+
+static bool	iter_input(t_smash *smash, int *start_token)
+{
+	int				iter;
+	t_token_type	type;
+
+	iter = -1;
+	type = -1;
+	while (smash->user_input[++iter])
+	{
+		if (is_start_token(*start_token, smash->user_input[iter]))
+		{
+			get_token_type(&type, &(smash->user_input[iter]));
+			*start_token = iter;
+			if (type == HEREDOC || type == APPEND)
+				iter++;
+		}
+		mutate(&type, smash->user_input[iter]);
+		if (is_end_token(*start_token, smash->user_input[iter + 1], type))
+		{
+			if (!add_token(smash, *start_token, iter - *start_token + 1, type))
+				return (false);
+			*start_token = -1;
+		}
 	}
 	return (true);
 }
